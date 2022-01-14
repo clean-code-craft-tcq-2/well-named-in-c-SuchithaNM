a70ea7bda83462a12bfbe23f7bf74ee40cf87b0c
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-01-14 17:15:35 +0530

    Update testcolorpair.h

--- testcolorpair.h
+++ testcolorpair.h
@@ -6,6 +6,8 @@
 extern void testNumberToPair(int pairNumber,    MajorColor_type expectedMajor,     MinorColor_type expectedMinor);
 extern void testPairToNumber( MajorColor_type major,     MinorColor_type minor,     int expectedPairNumber);
 
+
+
 #endif
 
 
