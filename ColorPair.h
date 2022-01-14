6eca49455ae14ce5f18124c5208e5125893e3907
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-01-14 17:18:32 +0530

    Update ColorPair.h

--- ColorPair.h
+++ ColorPair.h
@@ -11,8 +11,8 @@ const char* MajorColorNames[] = {
     "White", "Red", "Black", "Yellow", "Violet"
 };
 
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
+// int numberOfMajorColors =
+//     sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
 
 const char* MinorColorNames[] = {
     "Blue", "Orange", "Green", "Brown", "Slate"
@@ -20,8 +20,8 @@ const char* MinorColorNames[] = {
 
 const int MAX_COLORPAIR_NAME_CHARS = 16;
 
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
+// int numberOfMinorColors =
+//     sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
 
 typedef struct {
     MajorColor_type majorColor;
