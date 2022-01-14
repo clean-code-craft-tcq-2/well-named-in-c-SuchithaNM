a25bcc3c03f9a145a8537077814cbe5545498df4
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-01-14 12:05:27 +0530

    updates functions as extern

--- TestColor.h
+++ TestColor.h
@@ -20,3 +20,9 @@ typedef struct {
     enum MajorColor majorColor;
     enum MinorColor minorColor;
 } ColorPair;
+
+extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
+extern ColorPair GetColorFromPairNumber(int pairNumber);
+extern int GetPairNumberFromColor(const ColorPair* colorPair);
+extern void testNumberToPair(int pairNumber,    enum MajorColor expectedMajor,     enum MinorColor expectedMinor);
+extern void testPairToNumber(     enum MajorColor major,     enum MinorColor minor,     int expectedPairNumber);
