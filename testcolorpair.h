71e37c5563360460fbcbc24bc960faeb7be48bc9
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-01-14 12:46:03 +0530

    Update testcolorpair.h

--- testcolorpair.h
+++ testcolorpair.h
@@ -29,3 +29,6 @@ extern ColorPair GetColorFromPairNumber(int pairNumber);
 extern int GetPairNumberFromColor(const ColorPair* colorPair);
 extern void testNumberToPair(int pairNumber,    enum MajorColor expectedMajor,     enum MinorColor expectedMinor);
 extern void testPairToNumber(     enum MajorColor major,     enum MinorColor minor,     int expectedPairNumber);
+
+#endif
+
