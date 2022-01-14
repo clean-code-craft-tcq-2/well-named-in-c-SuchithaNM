006dd2f3fbf07bf3951f86b2508d9a8f36ddf29b
Suchitha N M <90901005+SuchithaNM@users.noreply.github.com>
2022-01-14 14:19:01 +0530

    Update testcolorpair.h

--- testcolorpair.h
+++ testcolorpair.h
@@ -1,36 +1,8 @@
 #ifndef TESTCOLORPAIR_H
 #define TESTCOLORPAIR_H
 
-#include <stdio.h>
-#include <assert.h>
+#include "ColorPair.h"
 
-typedef enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET} MajorColor_type;
-typedef enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE} MinorColor_type;
-
-const char* MajorColorNames[] = {
-    "White", "Red", "Black", "Yellow", "Violet"
-};
-
-int numberOfMajorColors =
-    sizeof(MajorColorNames) / sizeof(MajorColorNames[0]);
-
-const char* MinorColorNames[] = {
-    "Blue", "Orange", "Green", "Brown", "Slate"
-};
-
-const int MAX_COLORPAIR_NAME_CHARS = 16;
-
-int numberOfMinorColors =
-    sizeof(MinorColorNames) / sizeof(MinorColorNames[0]);
-
-typedef struct {
-    enum MajorColor majorColor;
-    enum MinorColor minorColor;
-} ColorPair;
-
-extern void ColorPairToString(const ColorPair* colorPair, char* buffer);
-extern ColorPair GetColorFromPairNumber(int pairNumber);
-extern int GetPairNumberFromColor(const ColorPair* colorPair);
 extern void testNumberToPair(int pairNumber,    MajorColor expectedMajor,     MinorColor expectedMinor);
 extern void testPairToNumber(     MajorColor major,     MinorColor minor,     int expectedPairNumber);
 
